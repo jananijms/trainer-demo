ba1349137c2bb45f17f0d6cc5ae1ddb526df37a8
jananijms <jananisundaram152gmail.com>
2022-12-13 14:08:12 +0530

    trainer commit

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var http = require("http");
 
-process.env.PORT || 3008
-
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Heroku world test 123!");
   response.end();
-}).listen(process.env.PORT);
+}).listen(3000);
